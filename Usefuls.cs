4a8ea3ad521532bd01379e3444186784d2ed7caf
FileEX <alkom@t.pl>
2023-04-25 23:49:28 +0200

    Remove unused using directives

--- Usefuls.cs
+++ Usefuls.cs
@@ -1,10 +1,4 @@
-﻿using LSPD_First_Response.Engine;
-using Rage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Rage;
 
 namespace PolishCallouts
 {
